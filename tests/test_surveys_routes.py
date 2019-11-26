73cdae0b007fcc5a215e2bb77589ec63ea81db62
Rachel Winkler <rachelwinkler9@gmail.com>
2019-11-26 22:40:56 +0000

    fixed tests for endpoints, changed mocked functions to mock correctly

--- tests/test_surveys_routes.py
+++ tests/test_surveys_routes.py
@@ -23,34 +23,42 @@ class TestSurveyRoutes(unittest.TestCase):
                             {"survey_id" : "820616", "survey_name" : "Thoughts on Dogs", "available_places" : 15, "user_id" : "5438"},
                             { "survey_id" : "292284", "survey_name" : "Opinions about Trees", "available_places" : 30, "user_id" : "2483"}]
 
-    @patch('Survey_API.run.get_all_surveys')
+    @patch('run.get_all_surveys')
     def test_route_returns_list_of_surveys_when_surveys_in_database(self, mock_all):
         mock_all.return_value = self.all_surveys
         result = self.app.get('/surveys')
-        print('CALL COUNT', mock_all.call_count)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.json, {'status': 'ok', 'data': self.all_surveys})
 
-    @patch('Survey_API.run.get_all_surveys')
+    @patch('run.get_all_surveys')
     def test_route_returns_info_message_when_no_surveys_in_database(self, mock_all):
         mock_all.return_value = None
         result = self.app.get('/surveys')
         self.assertEquals(result.status_code, 200)
         self.assertEqual(result.json, {'status': 'ok', 'data':'No surveys in database'})
 
-    @patch('Survey_API.run.SurveyBuilder.build')
-    @patch('Survey_API.run.Survey.create_survey')
-    @patch('Survey_API.run.Survey')
-    def test_route_creates_new_survey_and_adds_it_to_the_database(self, mock_survey, mock_create, mock_build):
+    @patch('run.SurveyBuilder.build')
+    @patch('run.Survey')
+    def test_route_creates_new_survey_and_adds_it_to_the_database(self, mock_survey, mock_build):
         mock_build.return_value = Mock()
-        mock_create.return_value = True
+        mock_build.return_value.create_survey.return_value = True
         mock_survey = Mock()
         mock_id = PropertyMock(return_value='123456')
         type(mock_survey).survey_id = mock_id
         result = self.app.post('/surveys', json=self.new_survey_data)
         self.assertEquals(result.status_code, 201)
 
-    @patch('Survey_API.run.SurveyResponse.create_survey_response')
+    @patch('run.SurveyBuilder.build')
+    @patch('run.Survey')
+    def test_route_returns_error_message_if_cannot_create_survey(self, mock_survey, mock_build):
+        mock_build.return_value = Mock()
+        mock_survey = Mock()
+        mock_build.return_value.create_survey.return_value = False
+        result = self.app.post('/surveys', json=self.new_survey_data)
+        self.assertEquals(result.status_code, 500)
+        self.assertEqual(result.json, {'status': 'fail', 'error': 'could not create new survey'})
+
+    @patch('run.SurveyResponse.create_survey_response')
     def test_route_checks_available_places_for_survey_response_and_creates_new_response_if_places_available(self, mock_create):
         mock_create.return_value = {'status': 'success'}
         result = self.app.post('/survey_responses', json=self.survey_response_success)
